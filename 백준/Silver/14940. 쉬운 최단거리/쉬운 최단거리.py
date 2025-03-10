af2e7cb8829048d3728215de13270eecf06ebbd8
aengzu <102356873+aengzu@users.noreply.github.com>
2025-03-10 18:47:01 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 512 ms, Memory: 43736 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-
+# 목표 지점 찾기
 def find_goal(grid):
     for i in range(n):
         for j in range(m):
@@ -21,6 +21,7 @@ def can_go(x, y):
     return True
 
 
+# 입력 받기
 n, m = tuple(map(int, input().split()))
 
 grid = [
@@ -33,10 +34,8 @@ goal_idx = find_goal(grid)
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 visited = [[False] * m for _ in range(n)]
-distances = [
-    [-1] * m for _ in range(n)
-]
-
+distances = [[0 if grid[i][j] == 0 else -1 for j in range(m)]
+             for i in range(n)]
 
 def bfs():
     q = deque()
@@ -45,21 +44,17 @@ def bfs():
     distances[goal_idx[0]][goal_idx[1]] = 0
 
     while q:
-        y, x = q.popleft()  # 행과 열
+        y, x = q.popleft()  # (행과 열)
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
+            # 갈 수 있는 곳이고 방문하지 않았다면 방문하기
             if can_go(nx, ny) and not visited[ny][nx]:
                 visited[ny][nx] = True
                 distances[ny][nx] = distances[y][x] + 1
                 q.append((ny, nx))
 
-
+# 그래프 순회
 bfs()
-
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] == 0:
-            distances[i][j] = 0
 
 # 결과 출력
 for row in distances:
