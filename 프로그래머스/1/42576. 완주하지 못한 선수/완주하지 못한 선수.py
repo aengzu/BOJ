7abe35a796c7a6926ac05a4c3ffc9f00224453a7
aengzu <102356873+aengzu@users.noreply.github.com>
2025-03-13 02:30:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.72 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def solution(participant, completion):
+    participant_counter = Counter(participant)
+    completion_counter = Counter(completion)
+    
+    for name in participant_counter:
+        if participant_counter[name] != completion_counter[name]:
+            return name
