2fc8d4bbe2919ff386d9b55c4cbf432319ed6ca5
aengzu <102356873+aengzu@users.noreply.github.com>
2025-09-25 21:57:37 +0900

    [level 1] Title: 실패율, Time: 9.62 ms, Memory: 9.87 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int, float>& a, pair<int, float>& b)
+{
+    if (a.second==b.second) {
+        return a.first < b.first;
+    } else {
+        return a.second > b.second;
+    }
+}
+
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
+    vector<float> challenger(N+2, 0.0);
+    vector<float> fail(N+2, 0.0);
+    
+    for (int i=0; i<stages.size(); i++)
+    {
+        for (int j=1; j<=stages[i]; j++)
+         challenger[j]++;
+        
+        fail[stages[i]]++;
+    }
+    
+    vector<pair<int, float>> failRatio(N);
+    
+    for (int i=0; i<N; i++) {
+        failRatio[i].first = i+1;
+        
+        if (challenger[i+1]==0)
+            failRatio[i].second = 0;
+        else
+            failRatio[i].second = fail[i+1] / challenger[i+1];
+    
+    }
+
+    sort(failRatio.begin(), failRatio.end(), compare);
+    
+    for (const auto& idx: failRatio)
+    {
+        answer.push_back(idx.first);
+    }
+    
+    return answer;
+}
