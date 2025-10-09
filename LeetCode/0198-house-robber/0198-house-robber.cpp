308df5bae4b8fb14a667b36bae97adc4d068e5b7
aengzu <102356873+aengzu@users.noreply.github.com>
2025-10-09 20:24:34 +0900

    Time: 0 ms (100%), Space: 10.9 MB (5.82%) - LeetHub

--- LeetCode/0198-house-robber/0198-house-robber.cpp
+++ LeetCode/0198-house-robber/0198-house-robber.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int sz = nums.size();
-
-        if (sz==0) return nums[0];
-        if (sz==1) return max(nums[0], nums[1]);
-
         vector<int> mon(sz, 0);
+
+        if (sz==1) return nums[0];
+        if (sz==2) return max(nums[0], nums[1]);
+
         mon[0] = nums[0];
         mon[1] = max(nums[0], nums[1]);
         for (int i=2; i<sz; ++i) {
